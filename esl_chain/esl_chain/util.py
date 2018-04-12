573118f7596236f21cb2e918d9f3c80a2080349a
ILHAN <jih941108@gmail.com>
2018-04-12 23:35:35 +0900

    edit Data type

--- esl_chain/esl_chain/util.py
+++ esl_chain/esl_chain/util.py
@@ -36,8 +36,8 @@ def LabelMaker(itemId, itemName, itemPrice, itemUrl):
     name_font = ImageFont.truetype("./img_esl/malgunbd.ttf", 32)
     price_font = ImageFont.truetype("./img_esl/ds-digit.ttf", 60)
 
-    draw.text((12, 6), itemName, font=name_font)
-    draw.text((16, 60), itemPrice, font=price_font)
+    draw.text((12, 6), str(itemName), font=name_font)
+    draw.text((16, 60), str(itemPrice), font=price_font)
 
     img.paste(qrimg,(189, 15))
 
