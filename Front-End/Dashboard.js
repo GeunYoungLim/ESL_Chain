fa9806ec77f734c314257bdfdc11ee1c0d66da24
sik2 <sksik622@naver.com>
2018-05-31 17:04:36 +0900

    p_veiw

--- Front-End/Dashboard.js
+++ Front-End/Dashboard.js
@@ -4,27 +4,27 @@ new Chart(document.getElementById("line-chart"), {
         labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
         datasets: [{
             data: [86,114,106,106,107,111,133,221,783,2478],
-            label: "Africa",
+            label: "A",
             borderColor: "#3e95cd",
             fill: false
         }, {
             data: [282,350,411,502,635,809,947,1402,3700,5267],
-            label: "Asia",
+            label: "B",
             borderColor: "#8e5ea2",
             fill: false
         }, {
             data: [168,170,178,190,203,276,408,547,675,734],
-            label: "Europe",
+            label: "C",
             borderColor: "#3cba9f",
             fill: false
         }, {
             data: [40,20,10,16,24,38,74,167,508,784],
-            label: "Latin America",
+            label: "D",
             borderColor: "#e8c3b9",
             fill: false
         }, {
             data: [6,3,2,2,7,26,82,172,312,433],
-            label: "North America",
+            label: "E",
             borderColor: "#c45850",
             fill: false
         }
@@ -33,7 +33,7 @@ new Chart(document.getElementById("line-chart"), {
     options: {
         title: {
             display: true,
-            text: 'World population per region (in millions)'
+            text: '채굴현황(in coin)'
         }
     }
 });
